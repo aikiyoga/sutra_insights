fc835ee55b1a9558c9ba2611f24d05c1596b52cc
Aiki Matsushita <aikimatsu@gmail.com>
2025-06-14 07:29:06 +0900

    added links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -46,6 +46,45 @@ export default function RootLayout({
             </main>
             <footer className="py-4 text-center">
               <p>© {new Date().getFullYear()} Sutra Insights</p>
+              <div style={{ overflowWrap: 'break-word' }} className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center mt-4">
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '10px', paddingBottom: '10px' }}>
+                  <span style={{ fontFamily: 'Verdana', fontWeight: 'bold' }}>
+                    Aiki Yoga
+                  </span>
+                  <a href="https://www.aiki-yoga.net/" style={{ textDecoration: 'none' }} title="Aiki Yoga Website">
+                    <img
+                      src="https://www.aiki-yoga.net/assets/img/lotus-flower.png"
+                      width="50"
+                      style={{ opacity: 0.3 }}
+                      alt="Aiki Yoga"
+                    />
+                  </a>
+                  <a href="https://www.instagram.com/aikimatsu.yoga" style={{ textDecoration: 'none' }} title="Aiki Yoga Instagram">
+                    <img
+                      src="https://www.aiki-yoga.net/assets/img/instagram.png"
+                      width="30"
+                      style={{ opacity: 0.3 }}
+                      alt="instagram"
+                    />
+                  </a>
+                  <a href="https://aikiyoga.substack.com" style={{ textDecoration: 'none' }} title="Aiki Yoga Substack">
+                    <img
+                      src="https://www.aiki-yoga.net/assets/img/substack.png"
+                      width="30"
+                      style={{ opacity: 0.3 }}
+                      alt="substack"
+                    />
+                  </a>
+                  <a href="https://x.com/aiki_yoga" style={{ textDecoration: 'none' }} title="Aiki Yoga Twitter/X">
+                    <img
+                      src="https://www.aiki-yoga.net/assets/img/twitter-x.png"
+                      width="30"
+                      style={{ opacity: 0.3 }}
+                      alt="Twitter/X"
+                    />
+                  </a>
+                </div>
+              </div>
             </footer>
           </div>
         </ThemeProvider>
